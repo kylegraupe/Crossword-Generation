20255b5608e6b947e2577449b253a9b9626bdf93
kylegraupe <91435626+kylegraupe@users.noreply.github.com>
2023-10-31 15:32:37 -0400

    Run both tasks simulatenously

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import backtracking
 import forward_checking
 
 if __name__ == '__main__':
-    # backtracking.execute_application()
+    print(f'TASK 1: SIMPLE BACKTRACKING..')
+    backtracking.execute_application()
+
+    print(f'TASK 2: BACKTRACKING WITH FORWARD CHECKING HEURISTIC..')
     forward_checking.print_outputs()
